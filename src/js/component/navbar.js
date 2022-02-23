b0664c4a49a3becda07a59769412dcd4edf60ad3
vitortamberlini <vitor.tamberlini@outlook.com>
2022-02-23 00:48:49 +0000

    WIP

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,28 @@
 import React from "react";
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Image from 'react-bootstrap/Image'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+			<Row xs={2} md={4} lg={6} className="bg-light justify-content-between align-items-center">
+				<Col>
+					<Link to="/">
+						<Image
+						className="img-fluid w-50 ms-3"
+						src="https://www.freepnglogos.com/uploads/star-wars-logo-31.png" />
+					</Link>
+				</Col>
+				<Col>
+				<DropdownButton size="lg" title="Favorites">
+					<Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+					<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+					<Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+				</DropdownButton>
+				</Col>
+			</Row>
 	);
 };
--- src/js/layout.js
+++ src/js/layout.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
@@ -10,16 +9,12 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
-//create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";
 
 	return (
-		<div>
+		<>
 			<BrowserRouter basename={basename}>
-				<ScrollToTop>
 					<Navbar />
 					<Switch>
 						<Route exact path="/">
@@ -36,9 +31,8 @@ const Layout = () => {
 						</Route>
 					</Switch>
 					<Footer />
-				</ScrollToTop>
 			</BrowserRouter>
-		</div>
+		</>
 	);
 };
 
